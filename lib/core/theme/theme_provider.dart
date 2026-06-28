05908f7bc3ca81de7449afd719b9964589bbbaeb
ALIFKA-HUB <alifkaareiga@gmail.com>
2026-06-28 22:40:13 +0700

    feat: init flutter project and design system

---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  static const String _themeKey = 'isDarkMode';
+  bool _isDarkMode = false;
+  late SharedPreferences _prefs;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _loadFromPrefs();
+  }
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    _saveToPrefs();
+    notifyListeners();
+  }
+
+  Future<void> _loadFromPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkMode = _prefs.getBool(_themeKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveToPrefs() async {
+    await _prefs.setBool(_themeKey, _isDarkMode);
+  }
+}
