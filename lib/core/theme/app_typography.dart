02337147d80183a4b2b3b58842e65990d755cd75
ALIFKA-HUB <alifkaareiga@gmail.com>
2026-07-01 12:46:40 +0700

    style(theme): enforce global Poppins typography

--- lib/core/theme/app_typography.dart
+++ lib/core/theme/app_typography.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTypography {
-  static TextStyle get heading => GoogleFonts.inter(
+  static TextStyle get heading => GoogleFonts.poppins(
         fontSize: 24,
         fontWeight: FontWeight.bold,
       );
 
-  static TextStyle get subHeading => GoogleFonts.inter(
+  static TextStyle get subHeading => GoogleFonts.poppins(
         fontSize: 18,
         fontWeight: FontWeight.w600,
       );
 
-  static TextStyle get body => GoogleFonts.inter(
+  static TextStyle get body => GoogleFonts.poppins(
         fontSize: 14,
         fontWeight: FontWeight.normal,
       );
 
-  static TextStyle get caption => GoogleFonts.inter(
+  static TextStyle get caption => GoogleFonts.poppins(
         fontSize: 12,
         fontWeight: FontWeight.normal,
       );
 
-  static TextStyle get metric => GoogleFonts.inter(
+  static TextStyle get metric => GoogleFonts.poppins(
         fontSize: 32,
         fontWeight: FontWeight.bold,
       );
 
-  static TextStyle get buttonLabel => GoogleFonts.inter(
+  static TextStyle get buttonLabel => GoogleFonts.poppins(
         fontSize: 14,
         fontWeight: FontWeight.w600,
       );
